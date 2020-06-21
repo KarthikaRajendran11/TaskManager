8fa48f0b0e8cd37e6f2509da833e518bcf647b83
Karthika Rajendran <karthika.punitha@gmail.com>
2020-06-21 12:50:24 -0400

    Add frisk validation for request payload (#4)

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,39 +3,58 @@ const express = require('express');
 const Note = require('../repository/notes.repository');
 const auth = require('../middleware/auth');
 const error = require('../util/error');
+const frisk = require('express-frisk');
 
 const router = express.Router();
 
-router.get('/notes', auth, async (req, res) => {
-    const size = req.query.size;
-    try {
-        await req.user.populate({
-            path: 'notes',
-            options: {
-                limit: size
-            }
-        }).execPopulate();
-        res.send(req.user.notes);
-    } catch(err) {
-        res.send(err).status(400);
+router.get('/notes', auth, frisk.validateRequest({
+        notes: {
+            required: false,
+            type: frisk.types.integer,
+            in: 'query'
+        }
+    }), 
+    async (req, res) => {
+        const size = req.query.size;
+        try {
+            await req.user.populate({
+                path: 'notes',
+                options: {
+                    limit: size
+                }
+            }).execPopulate();
+            res.send(req.user.notes);
+        } catch(err) {
+            res.send(err).status(400);
+        }
+        const notes = await Note.findNotesByOwner(req.user.id);
+        res.status(200).send(notes);
+});
+
+router.post('/notes', auth, frisk.validateRequest({
+        notes: {
+            required: true,
+            type: frisk.types.string,
+            in: 'body'
+        }
+    }),
+    async (req, res) => {
+        const response = await Note.insertNotesIntoDocuments(req.body.notes, req.user.id);
+        res.status(200).send(response);
+});
+
+router.patch('/notes', auth, frisk.validateRequest({
+    id: {
+        required: true,
+        type: frisk.types.integer,
+        in: 'body'
+    },
+    notes: {
+        required: true,
+        type: frisk.types.string,
+        in: 'body'
     }
-    const notes = await Note.findNotesByOwner(req.user.id);
-    res.status(200).send(notes);
-});
-
-router.post('/notes', auth, async (req, res) => {
-    // validate request body -> frisk
-    // rate limit ?
-    const response = await Note.insertNotesIntoDocuments(req.body.notes, req.user.id);
-    res.status(200).send(response);
-});
-
-router.patch('/notes', auth, async (req, res) => {
-    // validate if the correct feilds are being updated
-    const keys = _.keys(req.body);
-    keys.every((key) => {['notes', 'id'].includes(key)});
-
-    // find note id
+}), async (req, res) => {
     const id = (await Note.findNoteById(req.body.id)).id;
     const response = await Note.updateNote(id, req.body.notes);
     res.send(response).send(200);
